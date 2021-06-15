4c537c8bb26c917d6fa9a7a69243050cb0f15caa
willsower <>
2021-06-15 07:06:09 -0700

    End navigation with toggle finished
    Have basic navbar finished with toggle

--- imamu-room/components/nav.js
+++ imamu-room/components/nav.js
@@ -1,20 +1,30 @@
+import Image from "next/image";
+import { useState } from "react";
+
 export default function Nav() {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+
   return (
-    <nav className="flex items-center justify-between flex-wrap p-6 bg-black">
+    <nav className="flex items-center justify-between flex-wrap p-4 main-color">
+      {/* Logo */}
       <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <svg
-          className="fill-current h-8 w-8 mr-2"
-          width="54"
-          height="54"
-          viewBox="0 0 54 54"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-        </svg>
-        <span className="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+        <Image src="/icons/logo.jpg" alt="Logo" width={40} height={40} />
+        <span className="font-semibold text-xl tracking-tight ml-2">
+          Imamu room
+        </span>
       </div>
-      <div className="block lg:hidden">
-        <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+      {/* End of Logo */}
+
+      {/* Hamburger */}
+      <div className="block sm:hidden">
+        <button
+          className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white outline-none"
+          onClick={handleClick}
+        >
           <svg
             className="fill-current h-3 w-3"
             viewBox="0 0 20 20"
@@ -25,36 +35,36 @@ export default function Nav() {
           </svg>
         </button>
       </div>
-      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div className="text-sm lg:flex-grow">
+      {/* End of Hamburger */}
+
+      {/* Nav Options */}
+      <div
+        className={`${
+            active ? '' : 'hidden'
+          } w-full block flex-grow sm:flex sm:items-center sm:w-auto text-right`}
+      >
+        <div className="text-sm sm:flex-grow">
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            className="block mt-4 sm:inline-block sm:mt-0 text-white font-medium"
           >
-            Home
+            home
           </a>
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            className="block mt-4 sm:inline-block sm:mt-0 text-white ml-4 font-medium"
           >
-            Recipes
+            recipes
           </a>
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+            className="block mt-4 sm:inline-block sm:mt-0 text-white ml-4 font-medium"
           >
-            Contact
-          </a>
-        </div>
-        <div>
-          <a
-            href="#"
-            className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-          >
-            Download
+            contact
           </a>
         </div>
       </div>
+      {/* End of Nav Options */}
     </nav>
   );
 }
