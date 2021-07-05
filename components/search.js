4ac4fecae9a5d8c8459841acca713a374e883334
Taichen <jatr812@gmail.com>
2021-07-05 14:53:25 -0700

    Adding in search bar component

---
+++ components/search.js
@@ -0,0 +1,12 @@
+export default function Search() {
+  return (
+    <>
+      <div className="shadow flex">
+        <input className="w-full rounded p-2" type="text" placeholder="Search..." />
+        <button className="bg-white w-auto flex justify-end items-center text-blue-500 p-2 hover:text-blue-400">
+          <i className="material-icons">search</i>
+        </button>
+      </div>
+    </>
+  );
+}
