648798269da7d31d2d19b97ce662f0774873f3d1
Taichen <jatr812@gmail.com>
2021-07-05 14:24:09 -0700

    Got rid of layout

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,18 @@ import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 
 export default function Home() {
-  <Layout home>
-    <Head>
-      <title>{siteTitle}</title>
-    </Head>
-    <section className={utilStyles.headingMd}>
-      <p>[Your Self Introduction]</p>
-      <p>
-        (This is a sample website - you’ll be building a site like this on{" "}
-        <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-      </p>
-    </section>
-  </Layout>;
+  return (
+    <>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </>
+  );
 }
--- pages/recipes/first-recipe.js
+++ pages/recipes/first-recipe.js
@@ -1,10 +1,9 @@
 import Link from 'next/link'
 import Head from 'next/head'
-import Layout from '../../components/layout'
 
 export default function FirstRecipe() {
     return (
-        <Layout>
+        <>
           <Head>
             <title>First Post</title>
           </Head>
@@ -14,6 +13,6 @@ export default function FirstRecipe() {
               <a>Back to home</a>
             </Link>
           </h2>
-        </Layout>
+        </>
       )
   }
