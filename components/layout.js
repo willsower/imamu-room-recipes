947881f86dc99e8347314cf0474dda2f14422413
Taichen <jatr812@gmail.com>
2021-07-05 14:16:56 -0700

    Brought back home css, updating layout

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,72 @@
-import styles from './layout.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/layout.module.css';
+import Link from 'next/link';
 
-export default function Layout({ children }) {
-  return <div className={styles.container}>{children}</div>
+const name = 'Imamu-Room Recipes';
+export const siteTitle = 'Imamu-Room Recipes'
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={}
+              height={144}
+              width={144}
+              alt={name}
+            />
+            <h1 className={}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <Image
+                  priority
+                  src="/images/profile.jpg"
+                  className={}
+                  height={108}
+                  width={108}
+                  alt={name}
+                />
+              </a>
+            </Link>
+            <h2 className={}>
+              <Link href="/">
+                <a className={}>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  )
 }
