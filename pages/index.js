7733e4f57313719147be3a373d26b7a639bb664d
Taichen <jatr812@gmail.com>
2021-07-06 08:00:19 -0700

    Wrote recipe card function to make cards and a date month to parse the date

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Link from "next/link";
-import Image from "next/image";
 import Nav from "../components/Nav/nav";
 import Header from "../components/Header/header";
 import Footer from "../components/Footer/footer";
@@ -16,22 +15,44 @@ export async function getStaticProps() {
 }
 
 export default function Home({ allRecipeData }) {
-  function recipeCard(title, id, date, image) {
+  function getDate(date) {
+    var months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    var year = date.substr(0, 4);
+    var month = months[parseInt(date.substr(5, 2))];
+    var day = parseInt(date.substr(8, 2));
+
+    var newDate = month + " " + day + ", " + year;
+
+    return newDate;
+  }
+  function recipeCard(title, date, image) {
     return (
       <>
-      <div className = "block w-48 h-48 bg-indigo-300 p-2 mr-2 mb-2">
-        <Image 
-          src = {image}
-          width = "144px"
-          height = "144px"
-        />
-
-      {title}
-        <br />
-        {id}
-        <br />
-        {date}
-      </div>
+        <div className="block w-72 h-72 bg-white mr-2 mb-2 border rounded-lg relative">
+          <img
+            src={image}
+            className="w-full h-full object-cover opacity-75 hover:opacity-100 rounded-lg"
+          ></img>
+          <div className="absolute bottom-8 left-2 text-white">
+            <span className="font-bold">Recipe:</span> <span>{title}</span>
+            <br />
+            <span className="text-sm">{getDate(date)}</span>
+          </div>
+        </div>
       </>
     );
   }
@@ -48,10 +69,10 @@ export default function Home({ allRecipeData }) {
         <Nav />
       </div>
 
-      <section className="flex flex-wrap bg-red-300 h-auto ml-6 mr-6">
+      <section className="flex flex-wrap h-auto ml-6 mr-6 mt-4 pb-12">
         {allRecipeData.map(({ id, date, title, image }) => (
           <div className="" key={id}>
-            {recipeCard(title, id, date, image)}
+            {recipeCard(title, date, image)}
           </div>
         ))}
       </section>
