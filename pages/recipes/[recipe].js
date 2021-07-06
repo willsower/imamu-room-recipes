f509e5216101d3198389bad465de2c56bf4a67a7
Taichen <jatr812@gmail.com>
2021-07-06 09:24:28 -0700

    Added in static function to grab recipe id

--- pages/recipes/[recipe].js
+++ pages/recipes/[recipe].js
@@ -1,3 +1,13 @@
+import { getAllRecipeIds } from '../../lib/recipes'
+
+export async function getStaticPaths() {
+  const paths = getAllRecipeIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
 export default function Recipe() {
   return <></>;
 }
