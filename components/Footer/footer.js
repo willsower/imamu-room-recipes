4e08eb7cd9a117b3421039645d6661dd46b0e72f
Taichen <jatr812@gmail.com>
2021-07-06 05:52:10 -0700

    Implemented basic footer and added

--- components/Footer/footer.js
+++ components/Footer/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <div>
+      <div className = "text-center bg-gray-50 p-2 text-indigo-400">
         {/* About (link)
         Contributing (link) */}
         Copyright &copy; 2021
