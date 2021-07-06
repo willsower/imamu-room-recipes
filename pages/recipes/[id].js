1ec4269d26252c2435176d52d5bdab1dfb0308ca
Taichen <jatr812@gmail.com>
2021-07-06 14:21:53 -0700

    Got the recipe page roughly finished

--- pages/recipes/[id].js
+++ pages/recipes/[id].js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { getAllRecipeIds, getRecipeData } from "../../lib/recipes";
 import Header from "../../components/Header/header";
 import Nav from "../../components/Nav/nav";
+import Footer from "../../components/Footer/footer";
 import { getDate } from "../../lib/helper";
 
 export async function getStaticProps({ params }) {
@@ -22,10 +23,10 @@ export async function getStaticPaths() {
 }
 
 export default function Recipe({ recipeData }) {
-  console.log(recipeData)
+  console.log(recipeData);
 
   return (
-    <>
+    <div className="min-h-screen relative">
       <Head>
         <title>{recipeData.card_title}</title>
       </Head>
@@ -34,17 +35,48 @@ export default function Recipe({ recipeData }) {
       <Nav />
 
       {/* Body */}
-      <div className = "text-center w-10/12 m-auto">
+      <div className="text-center w-10/12 m-auto pb-16">
         {/* Title and Date */}
-        <h1 className = "mt-4 text-4xl">{recipeData.title}</h1>
+        <h1 className="mt-4 text-4xl">{recipeData.title}</h1>
         <p>{getDate(recipeData.date)}</p>
 
+        {/* Description */}
+        <div dangerouslySetInnerHTML={{ __html: recipeData.contentHtml }} />
+
         {/* Image */}
-        <div className = "lg:w-9/12 h-auto sm:h-96 md:h-96 m-auto mt-4">
-          <img src={recipeData.image} className="w-full h-full object-cover"></img>
+        <div className="lg:w-9/12 h-auto sm:h-96 md:h-96 m-auto mt-4">
+          <img
+            src={recipeData.image}
+            className="w-full h-full object-cover"
+          ></img>
+        </div>
+
+        {/* Ingredients */}
+        <div className="border p-2 mt-4">
+          <span className="text-2xl">Ingredients</span> <br />
+          <div className = "text-left p-4">
+            {recipeData.Ingredients.map((ingred) => (
+              <>
+                {ingred} <br />
+              </>
+            ))}
+          </div>
+        </div>
+
+        {/* Instructions */}
+        <div className="border p-2 mt-4">
+          <span className="text-2xl">Instructions</span> <br />
+          <div className = "text-left p-4">
+            {recipeData.Instructions.map((instruct) => (
+              <>
+                {instruct} <br />
+              </>
+            ))}
+          </div>
         </div>
       </div>
-      <div dangerouslySetInnerHTML={{ __html: recipeData.contentHtml }} />
-    </>
+
+      <Footer />
+    </div>
   );
 }
