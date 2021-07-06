14729ede214e48ca18eb609718fdaf0ca97c1f95
Taichen <jatr812@gmail.com>
2021-07-06 05:56:15 -0700

    Added in my footer

--- components/Footer/footer.js
+++ components/Footer/footer.js
@@ -1,11 +1,11 @@
 export default function Footer() {
   return (
-    <>
-      <div className = "text-center bg-gray-50 p-2 text-indigo-400">
+    <footer className = "">
+      <div className = "text-center bg-gray-50 p-2 text-indigo-400 fixed bottom-0 w-full">
         {/* About (link)
         Contributing (link) */}
         Copyright &copy; 2021
       </div>
-    </>
+    </footer>
   );
 }
