0b0cb499501f6e6b9c4f44ffc750e92eb50c6bd0
Taichen <jatr812@gmail.com>
2021-07-07 07:08:44 -0700

    Working on search api to look within array

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,11 +1,24 @@
-import { getSortedRecipesData } from '../../lib/recipes'
+import { getSortedRecipesData } from "../../lib/recipes";
 
-const recipes = process.env.NODE_ENV === 'production' ? require('../../cache/data').recipes : getSortedRecipesData()
+const recipes =
+  process.env.NODE_ENV === "production"
+    ? require("../../cache/data").recipes
+    : getSortedRecipesData();
 
 export default (req, res) => {
-  const results = req.query.q ?
-    recipes.filter(recipe => recipe.title.toLowerCase().includes(req.query.q)) : []
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ results }))
-}
+
+    const results = req.query.q
+      ? recipes.filter((recipe) =>
+          recipe.title.toLowerCase().includes(req.query.q.toLowerCase()) ||recipe.youtube_title.toLowerCase().includes(req.query.q.toLowerCase()) ||
+          recipe.card_title.toLowerCase().includes(req.query.q.toLowerCase()) || recipe.tags.filter((tag) => tag.toLowerCase().includes(req.query.q.toLowerCase())) || recipe.Ingredients.filter((ingred) => ingred.toLowerCase().includes(req.query.q.toLowerCase())))
+      : [];
+
+    // const results = req.query.q
+    // ? recipes.filter((recipe) =>
+    //     recipe.title.toLowerCase().includes(req.query.q.toLowerCase()) ||recipe.youtube_title.toLowerCase().includes(req.query.q.toLowerCase()) ||
+    //     recipe.card_title.toLowerCase().includes(req.query.q.toLowerCase()))
+    // : [];
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ results }));
+};
